350f1841f3aefa65acca5c8f83e919ddb85291c9
MisterBrash <matthew.brash@ocsb.ca>
2024-02-15 21:22:36 -0500

    Added some tests

--- tests/4U_1/1_1-Round.js
+++ tests/4U_1/1_1-Round.js
@@ -1,1 +1,19 @@
 // Testing the round(value, decimals) function.
+
+const round = require('./main');
+
+test('Return the rounded value 1', () => {
+  expect(round.round(3.1415926, 5)).toBe(3.14159);
+});
+
+test('Return the rounded value 2', () => {
+  expect(round.round(3.1415926, 4)).toBe(3.1416);
+});
+
+test('Return the rounded value 3', () => {
+  expect(round.round(3.1415926, 0)).toBe(3);
+});
+
+test('Return the rounded value 4', () => {
+  expect(round.round(3.1415926, 1)).toBe(3.1);
+});
