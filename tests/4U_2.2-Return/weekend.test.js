c1abca0c180b0de3d230eab34cb1544f310defb3
MisterBrash <matthew.brash@ocsb.ca>
2024-03-27 09:33:20 -0400

    Update weekend.test.js

--- tests/4U_2.2-Return/weekend.test.js
+++ tests/4U_2.2-Return/weekend.test.js
@@ -1,1 +1,19 @@
 /* Tests for is_it_the_weekend(day) */
+
+const w = require('./tasks_lesson_2');
+
+test('Is Monday a weekend?', () => {
+  expect(w.is_it_the_weekend(2)).toBe(false);
+});
+
+test('Is Sunday a weekend?', () => {
+  expect(w.is_it_the_weekend(1)).toBe(true);
+});
+
+test('Is Saturday a weekend?', () => {
+  expect(w.is_it_the_weekend(7)).toBe(true);
+});
+
+test('Is Thursday a weekend?', () => {
+  expect(w.is_it_the_weekend(5)).toBe(false);
+});
